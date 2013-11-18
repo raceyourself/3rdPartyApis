c9f33a5d0eb11662f0dcf3ebc67aed0d87789e21
husberg <janne@husberg.fi>
2013-11-18 16:50:11 +0000

    Once more with correct syntax.

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -4,7 +4,7 @@ require 'glassfit/positionsclient'
 class TrackController < ApplicationController 
 
   def show
-    @url = request.original_url.split(?).first
+    @url = request.original_url.split("?").first
     user = current_or_public_user()
     @track = Glassfit::TracksClient.new(user.doorkeeper_access_token).track(params[:id])
   end
